5a49d80126d238549a770870b62979948a41457f
Caden Jarrett <caden.jarrett99@gmail.com>
2022-05-12 10:22:21 -0600

    Test: lead actor test proving they have many movies

--- spec/models/lead_actor_spec.rb
+++ spec/models/lead_actor_spec.rb
@@ -4,4 +4,8 @@ RSpec.describe LeadActor, type: :model do
   describe "validations" do
     it { should validate_presence_of :name }
   end
+
+  describe 'relationships' do
+    it { should have_many :movies }
+  end
 end
