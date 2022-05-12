5afeec60cb6652708c54361f26bfd1c2c2905426
Caden Jarrett <caden.jarrett99@gmail.com>
2022-05-12 14:56:04 -0600

    Test: wrote test for lead actors index page feature

--- spec/models/movie_spec.rb
+++ spec/models/movie_spec.rb
@@ -3,10 +3,13 @@ require 'rails_helper'
 RSpec.describe Movie, type: :model do
   describe "validations" do
     it { should validate_presence_of :name }
+    it { should validate_presence_of :name }
+    it { should validate_presence_of :name }
+    it { should validate_presence_of :name }
+    it { should validate_presence_of :name }
   end
 
   describe 'relationships' do
-    # binding.pry
     it { should belong_to :lead_actor }
   end
 end
